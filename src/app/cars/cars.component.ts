6692de7d649320cbc75cbe77137e06fb9c0785cc
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-01-20 20:39:05 +0200

    Update

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { cars } from 'src/data/cars';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+  cars: any[] = cars;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
