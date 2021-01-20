d527e2578be00b1017b92450270956adc218afb0
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-01-20 20:36:57 +0200

    Update

---
+++ src/data/cars.ts
@@ -0,0 +1,9 @@
+export let cars = [
+  {brand: 'mercedes', model: 'cx500', color: 'black', power: 1.6, hp: 130},
+  {brand: 'bmw', model: 'm5', color: 'blue', power: 4.4, hp: 280},
+  {brand: 'subaru', model: 'outlander', color: 'white', power: 1.9, hp: 110},
+  {brand: 'lanos', model: 'daewoo', color: 'green', power: 1.5, hp: 88},
+  {brand: 'voltswagen', model: 'golf', color: 'red', power: 1.6, hp: 102},
+  {brand: 'audi', model: 'a4', color: 'red', power: 1.8, hp: 140},
+  {brand: 'renault', model: 'megane', color: 'black', power: 1.6, hp: 115},
+];
