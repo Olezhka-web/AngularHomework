c7e138baa88869ec9b8c8b84e05f603e4fbdaaae
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-01-23 16:57:14 +0200

    Update

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {Post} from '../../../models/Post';
+import {EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  @Input()
+  post: Post;
+  @Output()
+  bubbleUp = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getPostInfo(post: Post): void{
+    console.log(post);
+    this.bubbleUp.emit(post);
+  }
+}
